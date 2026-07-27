77c5d4b4190195305343d58801fb83ac6c13958c
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:21:36 +1200

    Create 2_09_AssignmentStatements.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_09_AssignmentStatements.py
@@ -0,0 +1,14 @@
+'''
+Assignment Statements
+'''
+
+a = 1
+b = c = 2
+[d, e] = [3, 4] # list
+(f, g) = (5, 6) # tuple
+h, i = 7
+#h, i = 7, 8
+
+print(a), print(b), print(c), print(d), print(e), print(f), print(g)
+print(a, b, c, d, e, f, g)
+print(h), print(i)
