27cf7a62062d3bfda5a664651162785427ff373e
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:21:18 +1200

    Create 2_04_Indentation.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_04_Indentation.py
@@ -0,0 +1,17 @@
+'''
+Indentation
+'''
+
+a = 1
+
+if a == 1:
+    result = "a is 1"
+#result = "a is 1"
+#print "this is an indentation example"
+else:
+    result = "a is not 1"   
+
+print (result)
+
+# this will cause an error because the indentation is not correct
+ print (a)
