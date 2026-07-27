495dde3b44277c17da8b1f372968f5dfa0f3049f
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:22:07 +1200

    Create 5_01_Print.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/5_01_Print.py
@@ -0,0 +1,24 @@
+'''
+5_01_Print.py
+'''
+
+a = 'COMPSYS'
+b = 302
+
+print (a)
+
+print (b)
+
+print (a), (b)
+print (a, b)
+print (a), print (b)
+
+print (a, type(a))
+print (b, type(b))
+print (str(b), type(str(b)))
+
+print (int(b), type(int(b)))
+
+g = lambda a, b : a*b
+print (type(g))
+
