1e460cd211f52e0036409c65bbb1a73df7eb84fb
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:21:33 +1200

    Create 2_08_2_Tuples.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_08_2_Tuples.py
@@ -0,0 +1,18 @@
+
+#Tuples
+
+a = (12345, )
+print ("a =", a)
+
+b = tuple ('hello')
+print ("b =", b)
+
+c = ("apple", "banana", "cherry")
+print ("c =", c)
+print ("c[0] =", c[0])
+print ("c[0:2] =", c[0:2]) 
+#==> start from the index 0, but not include the index 2 
+print ("c[-1] =", c[-1])
+
+#c[0] = "melon"
+#print ("c =", c)
