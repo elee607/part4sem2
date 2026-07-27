2548ff319d00291653572900d58684e1e2287b13
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:22:39 +1200

    Create 7_02_TryFinally.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/7_02_TryFinally.py
@@ -0,0 +1,13 @@
+'''
+7_02_TryFinally.py
+'''
+
+try:
+    c = 5 / 0
+finally:
+    print ("anyway execute")
+    # usually use it for closing resources
+
+#print ("anyway execute")   
+
+print("end")
