f80e608554eb5dd12b9b6e5eae6b06a811a66f75
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:23:42 +1200

    Create 2_03_Backslash.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_03_Backslash.py
@@ -0,0 +1,15 @@
+'''
+Backslash
+'''
+
+a = "Hi, you can join them into a logical line. For this, you can use a backslash (\) "
+b = "Hi,\
+ you can join them into a logical line.\
+ For this,\
+ you can use a backslash (\)"
+
+print(a)
+print(b)
+
+c = "back \\"
+print(c)
