41c3ad91ab424272c241fc2c2b95ff309ac9883c
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:21:31 +1200

    Create 2_08_1_Strings.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_08_1_Strings.py
@@ -0,0 +1,31 @@
+'''
+Strings
+'''
+
+a = 'string'
+b = "string"
+c = "This is a 'string'"
+
+print (a); print (b); print (c)
+
+
+d = 'I\'m a student'
+e = "I'm a student"
+
+print (d); print (e);
+
+
+g = " I'm a student\n\
+ of the University of Auckland"
+print (g)
+
+h = """ I'm a student
+ of the University of Auckland"""
+print (h)
+
+i = """ I'm a student\
+ of the University of Auckland"""
+print (i)
+
+
+
