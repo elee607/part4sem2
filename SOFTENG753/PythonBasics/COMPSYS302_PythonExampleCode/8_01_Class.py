88dfa23af876afbe9b160818c22139631e787983
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:22:44 +1200

    Create 8_01_Class.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/8_01_Class.py
@@ -0,0 +1,12 @@
+'''
+8_01_Class.py
+'''
+
+class Person:
+    Name = "default"
+
+p1 = Person()
+p2 = Person()
+
+print ("Name of p1 instance: " + p1.Name)
+print ("Name of p2 instance: " + p2.Name)
