fc9a807fac7ed7b61552fb88fd0ea1a995f976b0
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:21:40 +1200

    Create 2_11_StringOperations.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_11_StringOperations.py
@@ -0,0 +1,16 @@
+'''
+String Operations
+'''
+
+a = 'py' + 'thon'
+
+b = 'py' 'thon'
+c = 'py' * 3
+
+print(a, b, c)
+print(a[0], a[5])
+print(b[:], b[0:1], b[1:4], b[:2], b[-2:], b[::2])
+
+print(b[7:10])
+
+#a[0] = "a"
