05af8e8c58f373219b756ccbde28720fede6528e
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:23:37 +1200

    Create 2_01_Comments.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/2_01_Comments.py
@@ -0,0 +1,16 @@
+'''
+Comments
+'''
+
+# '#' is for comments
+''' for comments '''
+
+a = "'#' is for comment"
+b = "this is comment"
+print(a)
+print(b)
+c = a + b
+print(c)
+print ()
+d = 1
+print(f"{a} {b} test success {d}")
