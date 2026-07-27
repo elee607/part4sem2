23a05d166637e1e6cc3f6caa79b49cde194775c9
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:23:35 +1200

    Create 1_01_Welcome.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/1_01_Welcome.py
@@ -0,0 +1,11 @@
+'''
+Do not need to define type of variable
+'''
+
+a = "This is the first lecture on Python. Welcome."
+print (a)
+# This prints the value of variable a
+
+print("a")
+# This prints the string "a"
+
