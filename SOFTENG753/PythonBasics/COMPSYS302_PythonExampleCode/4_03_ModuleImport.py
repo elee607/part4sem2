5ec9c7b43bfc0941bd519f77f6b376dea40f8f22
elee607 <161412893+elee607@users.noreply.github.com>
2026-07-27 13:22:03 +1200

    Create 4_03_ModuleImport.py

---
+++ SOFTENG753/PythonBasics/COMPSYS302_PythonExampleCode/4_03_ModuleImport.py
@@ -0,0 +1,12 @@
+'''
+4_03_ModuleImport.py
+'''
+'''
+import DefineFunction 
+print (DefineFunction.Times(3, 4))
+'''
+
+import DefineFunction as myModule
+#print (DefineFunction.Times(3, 4))
+print (myModule.Times(10, 20))
+
